cad78cf7dc5f24b683f795a6dde6f4ad761f9544
MathCoder10 <81539939+MathCoder10@users.noreply.github.com>
2022-01-24 16:25:37 -0800

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -34,9 +34,3 @@ function redirectToRoomName(name){
   localStorage.setItem("room_name", name);
   window.location = "kwitter_page.html";
 }
-
-function logout() {
-  localStorage.removeItem("user_name");
-  localStorage.removeItem("room_name");
-  window.location = "index.html";
-}
